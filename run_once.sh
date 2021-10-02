d3e0110bac7c9ee45f066f809071f00782c8dc71
Nix <nix6839@washnix.com>
2021-10-02 19:41:18 +0900

    Add CHEZMOI_UTIL_PATH variable

--- run_once.sh
+++ run_once.sh
@@ -3,5 +3,6 @@
 
 export CHEZMOI_RESOURCE_PATH="$(chezmoi source-path)/resource"
 export CHEZMOI_SCRIPT_PATH="$CHEZMOI_RESOURCE_PATH/run_once_script"
+export CHEZMOI_UTIL_PATH="$CHEZMOI_SCRIPT_PATH/util"
 
 "$CHEZMOI_SCRIPT_PATH/prepare.sh"
