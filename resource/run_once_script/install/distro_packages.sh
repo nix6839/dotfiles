45cd1b4964861db3c070c372f3e34bd919a4793c
Nix <nix6839@washnix.com>
2021-10-05 03:39:29 +0900

    Update many things

--- resource/run_once_script/install/distro_packages.sh
+++ resource/run_once_script/install/distro_packages.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 sudo pacman --noconfirm -Syu
 sudo pacman --needed --noconfirm -S \
-  alacritty bluez chromium clang curl firefox firefox-i18n-ko gnupg i3-gaps nomacs \
-  noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra rofi smplayer \
-  smplayer-themes telegram-desktop ttf-jetbrains-mono xclip xdg-utils xorg-server \
-  xorg-xinit
+  alacritty bluez chromium clang curl firefox firefox-i18n-ko flameshot gnupg \
+  i3-gaps nomacs noto-fonts noto-fonts-cjk noto-fonts-extra rofi smplayer \
+  smplayer-themes telegram-desktop xclip xdg-utils xorg-server xorg-xinit
 
 install_makepkg() {
   url=$1
@@ -23,8 +22,9 @@ install_makepkg 'https://github.com/nix6839/my-fonts.git'
 curl -O 'https://linux.dropbox.com/fedora/rpm-public-key.asc' && \
   gpg --import rpm-public-key.asc && rm -f rpm-public-key.asc
 paru --needed --noconfirm -S \
-  discord_arch_electron dropbox kime notion-app bitwarden polybar \
-  systemd-boot-pacman-hook ttf-pretendard visual-studio-code-bin zscroll-git
+  discord_arch_electron dropbox kime nerd-fonts-jetbrains-mono notion-app bitwarden \
+  polybar systemd-boot-pacman-hook ttf-pretendard ttf-twemoji \
+  visual-studio-code-bin zscroll-git
 paru --noconfirm --clean
 if [ -f "$HOME/.npmrc" ]; then
   rm -f "$HOME/.npmrc"
