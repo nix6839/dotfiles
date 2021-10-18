2150911c298fbc33b48bf3a985072816f284b555
Nix <nix6839@washnix.com>
2021-10-18 15:52:36 +0900

    Update many things

--- private_dot_mozilla/private_firefox/private_dotfiles.default/private_user.js
+++ private_dot_mozilla/private_firefox/private_dotfiles.default/private_user.js
@@ -1,3 +1,4 @@
-user_pref('browser.fullscreen.autohide', false);
-user_pref('extensions.pocket.enabled', false);
-user_pref('browser.compactmode.show', true);
+user_pref("browser.fullscreen.autohide", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("browser.compactmode.show", true);
+user_pref("ui.key.menuAccessKeyFocuses", false);
