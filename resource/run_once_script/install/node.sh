6c4b99e9e25c48c6337ce91dd0b633c8a61682ed
Nix <nix6839@washnix.com>
2021-10-23 22:49:43 +0900

    Add fixme line

--- resource/run_once_script/install/node.sh
+++ resource/run_once_script/install/node.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+# FIXME: Change install script path https://github.com/nvm-sh/nvm/pull/2637
 mkdir "$NVM_DIR" && curl -o- 'https://raw.githubusercontent.com/nix6839/nvm/no-pager/install.sh' | \
   bash && . "$NVM_DIR/nvm.sh" && nvm install 16
