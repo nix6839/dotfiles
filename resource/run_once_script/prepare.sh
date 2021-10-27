0e7dccdca26d30353150704b81a2cfb37a22565d
Nix <nix6839@washnix.com>
2021-10-27 11:35:47 +0900

    Add mkdir about node repl history

--- resource/run_once_script/prepare.sh
+++ resource/run_once_script/prepare.sh
@@ -7,4 +7,5 @@ uncomment 'VerbosePkgLists' '/etc/pacman.conf'
 uncomment 'ParallelDownloads = 5' '/etc/pacman.conf'
 
 dirname "$LESSKEY" "$LESSHISTFILE" | xargs -L 1 mkdir -p
+dirname "$NODE_REPL_HISTORY" | xargs mkdir -p
 mkdir -p "$GNUPGHOME" && chmod 700 "$GNUPGHOME"
