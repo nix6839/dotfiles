2fe5dfe0a32424e96b318810389bc843c3533e6e
Nix <nix6839@washnix.com>
2021-11-05 08:23:58 +0900

    Update firefox use autoscroll

--- private_dot_mozilla/private_firefox/private_dotfiles.default/private_user.js
+++ private_dot_mozilla/private_firefox/private_dotfiles.default/private_user.js
@@ -2,3 +2,4 @@ user_pref("browser.fullscreen.autohide", false);
 user_pref("extensions.pocket.enabled", false);
 user_pref("browser.compactmode.show", true);
 user_pref("ui.key.menuAccessKeyFocuses", false);
+user_pref("general.autoScroll", true);
