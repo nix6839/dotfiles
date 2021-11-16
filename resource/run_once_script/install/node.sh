6158a3265e4feab93b4d5955db907153631d7321
Nix <nix6839@washnix.com>
2021-11-17 07:54:06 +0900

    Remove yarn and install packages by npm

--- resource/run_once_script/install/node.sh
+++ resource/run_once_script/install/node.sh
@@ -2,5 +2,5 @@
 
 # FIXME: Change install script path https://github.com/nvm-sh/nvm/pull/2637
 mkdir "$NVM_DIR" && curl -o- 'https://raw.githubusercontent.com/nix6839/nvm/no-pager/install.sh' | \
-  bash && . "$NVM_DIR/nvm.sh" && nvm install 16 && npm install -g yarn && \
-  yarn global add create-react-app
+  bash && . "$NVM_DIR/nvm.sh" && nvm install 16 && \
+  npm install -g create-react-app serve nodemon
