58613abd7072f390633bd53055de59fa76099889
Nix <nix6839@washnix.com>
2021-11-29 12:50:28 +0900

    Update node packages

--- resource/run_once_script/install/node.sh
+++ resource/run_once_script/install/node.sh
@@ -3,4 +3,4 @@
 # FIXME: Change install script path https://github.com/nvm-sh/nvm/pull/2637
 mkdir "$NVM_DIR" && curl -o- 'https://raw.githubusercontent.com/nix6839/nvm/no-pager/install.sh' | \
   bash && . "$NVM_DIR/nvm.sh" && nvm install 16 && \
-  npm install -g create-react-app serve nodemon
+  npm install -g typescript create-react-app pm2 serve nodemon
