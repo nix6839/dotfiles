a31a3f4b7d2d9c00628883ef3f7ab6c3d2575dbc
Nix <nix6839@washnix.com>
2022-02-04 16:31:19 +0900

    Fix typo

--- resource/run_once_script/prepare.sh
+++ resource/run_once_script/prepare.sh
@@ -9,6 +9,6 @@ uncomment 'ParallelDownloads = 5' '/etc/pacman.conf'
 dirname "$LESSKEY" "$LESSHISTFILE" | xargs -L 1 mkdir -p
 dirname "$NODE_REPL_HISTORY" | xargs mkdir -p
 dirname "$PYTHONHISTORY" | xargs mkdir -p && \
-  touch "$PYTHONHISTORY" &&  echo '# init' > "$PYTHONHISTORY"
+  touch "$PYTHONHISTORY" && echo '# init' > "$PYTHONHISTORY"
 mkdir -p "$GNUPGHOME" && chmod 700 "$GNUPGHOME"
 mkdir -p "$XDG_DATA_HOME/zsh/completions"
