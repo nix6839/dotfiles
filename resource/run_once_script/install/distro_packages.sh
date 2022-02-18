3de2a9b80a7c407e143036f6875b60ef35965be3
Nix <nix6839@washnix.com>
2022-02-18 13:06:31 +0900

    Update archlinux packages

--- resource/run_once_script/install/distro_packages.sh
+++ resource/run_once_script/install/distro_packages.sh
@@ -3,12 +3,12 @@
 sudo pacman --noconfirm -Syu
 sudo pacman --needed --noconfirm -S \
   sway xorg-xwayland wl-clipboard grim slurp jq xdg-desktop-portal-wlr \
-  xdg-utils xdg-user-dirs  \
+  xdg-utils xdg-user-dirs \
   pipewire pipewire-alsa pipewire-pulse pavucontrol bluez blueman playerctl \
   noto-fonts noto-fonts-cjk noto-fonts-extra ttf-jetbrains-mono alacritty \
-  clang python curl gnupg \
-  firefox-developer-edition firefox-developer-edition-i18n-ko \
-  chromium smplayer smplayer-themes telegram-desktop unzip
+  clang python curl gnupg unzip man docker docker-compose mkcert bind \
+  firefox-developer-edition firefox-developer-edition-i18n-ko chromium \
+  smplayer smplayer-themes telegram-desktop calibre
 
 install_makepkg() {
   url=$1
@@ -27,10 +27,9 @@ install_makepkg 'https://github.com/nix6839/my-fonts.git'
 curl -O 'https://linux.dropbox.com/fedora/rpm-public-key.asc' && \
   gpg --import rpm-public-key.asc && rm -f rpm-public-key.asc
 paru --needed --noconfirm -S \
-  clipman discord_arch_electron dropbox kime nerd-fonts-jetbrains-mono \
-  notion-app bitwarden marktext numix-circle-icon-theme-git psst \
-  rofi-lbonn-wayland systemd-boot-pacman-hook ttf-pretendard ttf-twemoji \
-  wl-clipboard-x11 insomnia visual-studio-code-bin
+  clipman dropbox kime nerd-fonts-jetbrains-mono notion-app bitwarden marktext \
+  numix-circle-icon-theme-git rofi-lbonn-wayland systemd-boot-pacman-hook \
+  ttf-pretendard ttf-twemoji wl-clipboard-x11 insomnia visual-studio-code-bin
 paru --noconfirm --clean
 if [ -f "$HOME/.npmrc" ]; then
   rm -f "$HOME/.npmrc"
