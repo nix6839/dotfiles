ae5c11a3d3b1c20612371956ed98a346747f5785
Nix <nix6839@washnix.com>
2022-03-17 01:07:37 +0900

    Add global npm package

--- resource/run_once_script/install/node.sh
+++ resource/run_once_script/install/node.sh
@@ -3,4 +3,4 @@
 mkdir "$NVM_DIR" && curl -o- 'https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh' | \
   bash && . "$NVM_DIR/nvm.sh" && nvm install --lts && npm i -g npm && \
   npm i -g create-next-app create-react-app @nestjs/cli \
-    nodemon localtunnel typescript npm-check-updates
+    typescript ts-node nodemon localtunnel npm-check-updates
