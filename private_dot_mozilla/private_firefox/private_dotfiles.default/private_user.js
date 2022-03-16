5870cc2b8309365a522f1379d7a8730353d27f83
Nix <nix6839@washnix.com>
2022-03-17 00:55:39 +0900

    Update many things

--- private_dot_mozilla/private_firefox/private_dotfiles.default/private_user.js
+++ private_dot_mozilla/private_firefox/private_dotfiles.default/private_user.js
@@ -1,7 +1,8 @@
-user_pref("browser.fullscreen.autohide", false);
-user_pref("extensions.pocket.enabled", false);
-user_pref("browser.compactmode.show", true);
-user_pref("ui.key.menuAccessKeyFocuses", false);
-user_pref("general.autoScroll", true);
-user_pref("font.name.monospace.ko", "JetBrains Mono NL");
-user_pref("font.name.monospace.x-western", "JetBrains Mono NL");
+user_pref('browser.fullscreen.autohide', false);
+user_pref('extensions.pocket.enabled', false);
+user_pref('browser.compactmode.show', true);
+user_pref('ui.key.menuAccessKeyFocuses', false);
+user_pref('general.autoScroll', true);
+user_pref('middlemouse.paste', false);
+user_pref('font.name.monospace.ko', 'JetBrains Mono NL');
+user_pref('font.name.monospace.x-western', 'JetBrains Mono NL');
