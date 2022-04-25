9bd8075aaf04e92ca8ede5e120d985df8203bcd5
Nix <nix6839@washnix.com>
2022-04-25 09:37:17 +0900

    Add create-vite

--- resource/run_once_script/install/node.sh
+++ resource/run_once_script/install/node.sh
@@ -2,5 +2,5 @@
 
 mkdir "$NVM_DIR" && curl -o- 'https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh' | \
   bash && . "$NVM_DIR/nvm.sh" && nvm install --lts && npm i -g npm && \
-  npm i -g create-next-app create-react-app @nestjs/cli \
+  npm i -g create-next-app create-vite create-react-app @nestjs/cli \
     typescript ts-node nodemon localtunnel npm-check-updates
