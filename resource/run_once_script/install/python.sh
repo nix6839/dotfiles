a147e0da97832cd2db099d950a7283af99ad2ee8
Nix <nix6839@washnix.com>
2022-06-30 09:39:41 +0900

    Install poetry

--- resource/run_once_script/install/python.sh
+++ resource/run_once_script/install/python.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-init_python_environment() {
-  pip install --upgrade pip && pip install autopep8 pylint
-}
-
-DJANGO_DEV_VENV="$XDG_DATA_HOME/python/virtualenvs/djangodev"
-
-init_python_environment && python -m venv "$DJANGO_DEV_VENV" &&
-  . "$DJANGO_DEV_VENV/bin/activate" && init_python_environment &&
-  pip install django && deactivate
+pip install --upgrade pip && pip install autopep8 pylint &&
+  curl -sSL https://install.python-poetry.org | python3 -
