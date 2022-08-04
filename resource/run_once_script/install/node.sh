dc9c5278671aed3d36b786f0bc578b3b0fbe9d72
Han Yeong-woo <nix6839@washnix.com>
2022-08-05 08:24:59 +0900

    Add sort-package-json

--- resource/run_once_script/install/node.sh
+++ resource/run_once_script/install/node.sh
@@ -4,4 +4,4 @@ mkdir "$NVM_DIR" && curl -o- 'https://raw.githubusercontent.com/nvm-sh/nvm/v0.39
   bash && . "$NVM_DIR/nvm.sh" && nvm install --lts --latest-npm &&
   corepack prepare pnpm@7.8.0 --activate && pnpm i -g create-next-app \
   create-vite create-turbo @nestjs/cli typescript ts-node nodemon localtunnel \
-  npm-check-updates
+  npm-check-updates sort-package-json
