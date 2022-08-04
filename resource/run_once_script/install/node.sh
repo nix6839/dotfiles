4c09ecc9f099156523d749c24d2f681b7e574440
Han Yeong-woo <nix6839@washnix.com>
2022-08-04 21:49:03 +0900

    Use pnpm instead of npm

--- resource/run_once_script/install/node.sh
+++ resource/run_once_script/install/node.sh
@@ -2,5 +2,6 @@
 
 mkdir "$NVM_DIR" && curl -o- 'https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh' |
   bash && . "$NVM_DIR/nvm.sh" && nvm install --lts --latest-npm &&
-  npm i -g create-next-app create-vite create-react-app @nestjs/cli \
-    typescript ts-node nodemon localtunnel npm-check-updates
+  corepack prepare pnpm@7.8.0 --activate && pnpm i -g create-next-app \
+  create-vite create-turbo @nestjs/cli typescript ts-node nodemon localtunnel \
+  npm-check-updates
